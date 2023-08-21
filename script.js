086cb56d343ddf77034ba7f41e38834f79714300
Darko <darkogligorijevic4@gmail.com>
2023-08-21 22:19:30 +0200

    Change start sign icon, and add some js logic

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ const modal = document.getElementById("myModal");
 const introSection = document.querySelector('.intro');
 const mainSection = document.querySelector('.main');
 
+const selectXEl = document.querySelector('.choice--x');
+const selectOEl = document.querySelector('.choice--o');
 const startGameEl = document.querySelector('.btn--intro');
 
-startGameEl.addEventListener('click', (e) => {
+selectXEl.addEventListener('click', function(e) {
+  toggleSignSelect.call(this, selectOEl);
+  
+})
+
+selectOEl.addEventListener('click', function(e)  {
+  toggleSignSelect.call(this, selectXEl);
+})
+
+function toggleSignSelect(hideEl) {
+  this.classList.add('choiced');
+  hideEl.classList.remove('choiced');
+}
+
+startGameEl.addEventListener('click',(e) => {
   startGame();
 })
+
+
+
+
 
 function startGame() {
   introSection.classList.add('hidden');
